8688034d287f71290498a087f0d3a5f555d1279f
John Leintz <johnleintz@scaledflow.com>
2020-06-29 16:09:26 -0500

    fixed model code.

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -11,45 +11,39 @@ var burger = require("../models/burger.js");
 console.log("made it to burgers_controller.js in controllers after require");
 
 // test test connection
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "Timber8293$@",
-  database: "burger_db",
-});
-
-// Create all our routes and set up logic within those routes where required.
-router.get("/", function (req, res) {
-  connection.query("SELECT * FROM burgers;", function (err, data) {
-    if (err) {
-      return res.status(500).end();
-    }
-
-    console.log(data);
-
-    res.render("index", { burgers: data });
-  });
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "Timber8293$@",
+//   database: "burger_db",
+// });
 
 // Create all our routes and set up logic within those routes where required.
 // router.get("/", function (req, res) {
-//   burger.all(function (data) {
-//     var hbsObject = {
-//       burger: data,
-//     };
-//     console.log(hbsObject);
-//     res.render("index", hbsObject);
+//   connection.query("SELECT * FROM burgers;", function (err, data) {
+//     if (err) {
+//       return res.status(500).end();
+//     }
+
+//     console.log(data);
+
+//     res.render("index", { burgers: data });
 //   });
 // });
 
-// burger.all(function (data) {
-//   var hbsObject = {
-//     burger: data,
-//   };
-//   console.log(hbsObject);
-//   res.render("index", hbsObject);
-// });
+// Create all our routes and set up logic within those routes where required.
+router.get("/", function (req, res) {
+  console.log("made it to router.get / in burgers_controller.js");
+  burger.all(function (data) {
+    var hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render("index", hbsObject);
+  });
+});
+
 console.log("made it to router.get");
 //console.log(req);
 
